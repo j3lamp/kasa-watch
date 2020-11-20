77d84858ed2adade5ab9d07a0d87c17a900d61fa
John Lamp <j3lamp@gmail.com>
2020-11-19 22:03:24 -0500

    Rwead configuration values from files
    Read configuration values from files that are specified form the
    command line. This is now generic enough to be generally useful.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict"
 
+const fs      = require("fs").promises;
+const process = require("process");
+
 const bent     = require("bent");
 const {Client} = require("tplink-smarthome-api");
+const yargs    = require("yargs/yargs");
 
 
 class KasaWatcher
@@ -97,7 +101,7 @@ class KasaWatcher
 async function run(a_home_assistant_url,
                    a_home_assistant_token,
                    a_switch_groups,
-                   a_interval_length)
+                   a_interval_length_ms)
 {
     try
     {
@@ -116,7 +120,7 @@ async function run(a_home_assistant_url,
             async() => {
                 watcher.checkAllAndUpdate();
             },
-            a_interval_length);
+            a_interval_length_ms);
     }
     catch (error)
     {
@@ -124,10 +128,246 @@ async function run(a_home_assistant_url,
     }
 }
 
-run("http://192.168.1.2",
-    "",
-    {"study_lights": {hosts:["192.168.1.10",
-                             "192.168.1.11",
-                             "192.168.1.12"],
-                      default_state: "off"}},
-    1000);
+async function readFile(file_path, encoding="utf8")
+{
+    let file = null;
+    try
+    {
+        file = await fs.open(file_path, 'r')
+        return await file.readFile({encoding: encoding});
+    }
+    catch (error)
+    {
+        return false;
+    }
+    finally
+    {
+        if (file)
+        {
+            await file.close()
+        }
+    }
+}
+
+async function main(argv)
+{
+    const INVALID_CLI_ARGS           =  1
+    const CANNOT_READ_TOKEN_FILE     =  2;
+    const CANNOT_READ_CONFIG_FILE    =  3;
+    const CANNOT_PARSE_CONFIG_FILE   =  4;
+    const CONFIGRUATION_INVALID      =  5
+    const MISSING_HA_URL             =  6;
+    const INVALID_HA_URL             =  7;
+    const MISSING_POLL_INTERVAL      =  8;
+    const INVALID_POLL_INTERVAL      =  9;
+    const NON_NUMERIC_POLL_INTERVAL  = 10;
+    const NOT_POSITIVE_POLL_INTERVAL = 11;
+    const MISSING_BINARY_SENSORS     = 12;
+    const EMPTY_BINARY_SENSORS       = 13;
+    const INVALID_BINARY_SENSORS     = 14;
+
+    const parsed_options = yargs(argv)
+          .options({
+              "ha-token-file": {
+                  demandOption: true,
+                  type:         "string",
+                  description:
+                  ("The path to the file containing the Home Assistant (HA) " +
+                   "long-lived access token")
+              },
+              "configuration": {
+                  alias:        "config",
+                  demandOption: true,
+                  type:         "string",
+                  description:
+                  ("The path the JSON configuration file.")
+              }
+          })
+          .epilog("A simple client that will watch the specified TP-Link Kasa " +
+                  "devices and update the associated Home Assistant binary "    +
+                  "sensor when one of the device's state changes."              +
+                  ``                                                                      + `\n` +
+                  ``                                                                      + `\n` +
+                  `Error Codes:`                                                          + `\n` +
+                  `  Invalid command line arguments:        ${INVALID_CLI_ARGS}`          + `\n` +
+                  `  Cannot read the HA token file:         ${CANNOT_READ_TOKEN_FILE}`    + `\n` +
+                  `  Cannot read the configuration file:    ${CANNOT_READ_CONFIG_FILE}`   + `\n` +
+                  `  Cannot parse the configuration file:   ${CANNOT_PARSE_CONFIG_FILE}`  + `\n` +
+                  `  Configuration file invalid:            ${CONFIGRUATION_INVALID}`     + `\n` +
+                  `  Configuration missing HA URL:          ${MISSING_HA_URL}`            + `\n` +
+                  `  Invalid HA URL:                        ${INVALID_HA_URL}`            + `\n` +
+                  `  Configuration missing poll interval:   ${MISSING_POLL_INTERVAL}`     + `\n` +
+                  `  Invalid poll interval:                 ${INVALID_POLL_INTERVAL}`     + `\n` +
+                  `  Non-numeric poll interval:            ${NON_NUMERIC_POLL_INTERVAL}`  + `\n` +
+                  `  Poll interval is not positive:        ${NOT_POSITIVE_POLL_INTERVAL}` + `\n` +
+                  `  Configuration missing binary sensors: ${MISSING_BINARY_SENSORS}`     + `\n` +
+                  `  Binary sensors object empty:          ${EMPTY_BINARY_SENSORS}`       + `\n` +
+                  `  Invalid binary sensors:               ${INVALID_BINARY_SENSORS}`     + `\n`
+                 );
+    const args = parsed_options.argv;
+
+    const ha_token = (await readFile(args["ha-token-file"])).trim();
+    if (!ha_token)
+    {
+        console.error(`Could not read the Home Assistnat token file ` +
+                      `"${args["ha-token-file"]}".`);
+        process.exit(CANNOT_READ_TOKEN_FILE);
+    }
+
+    const raw_configuration = await readFile(args["configuration"]);
+    if (!raw_configuration)
+    {
+        console.error(`Could not read the JSON configuration file ` +
+                      `"${args["configuration"]}".`);
+        process.exit(CANNOT_READ_CONFIG_FILE);
+    }
+    const configuration = (() => {
+        try
+        {
+            return JSON.parse(raw_configuration);
+        }
+        catch (error)
+        {
+            console.error(`Could not parse the JSON configuration file ` +
+                          `"${args["configuration"]}".`);
+            console.error(error.message);
+            process.exit(CANNOT_PARSE_CONFIG_FILE);
+        }
+    })();
+
+    if (Object.keys(configuration).length == 0)
+    {
+        console.error(`Configuration must be an object with the keys ` +
+                      `"home_assistant_url", "poll_interval_ms", and ` +
+                      `"binary_sensors".`);
+        process.exit(CONFIGRUATION_INVALID);
+    }
+
+    if (configuration["home_assistant_url"] === undefined)
+    {
+        console.error(`Configuration is missing the "home_assistant_url" entry.`);
+        process.exit(MISSING_HA_URL);
+    }
+    else if (!configuration["home_assistant_url"])
+    {
+        console.error(`The configuration value for "home_assistant_url" is ` +
+                      `required must be a non-empty string.`);
+        process.exit(INVALID_HA_URL);
+    }
+
+    if (configuration["poll_interval_ms"] === undefined)
+    {
+        console.error(`Configuration is missing the "poll_interval_ms" entry.`);
+        process.exit(MISSING_POLL_INTERVAL);
+    }
+    else if (!configuration["poll_interval_ms"])
+    {
+        console.error(`The configuration value for "poll_interval_ms" is ` +
+                      `required and must be a positive number.`);
+        process.exit(INVALID_POLL_INTERVAL);
+    }
+    else if (typeof(configuration["poll_interval_ms"]) != "number" &&
+             !(configuration["poll_interval_ms"] instanceof Number))
+    {
+        console.error(`The configuration value for "poll_interval_ms" must ` +
+                      `be a positive number.`);
+        process.exit(NON_NUMERIC_POLL_INTERVAL);
+    }
+    else if (configuration["poll_interval_ms"] <= 0)
+    {
+        console.error(`The configuration value for "poll_interval_ms" is ` +
+                      `less than or equal to zero, but must be a positive number.`);
+        process.exit(NOT_POSITIVE_POLL_INTERVAL);
+    }
+
+    if (configuration["binary_sensors"] === undefined)
+    {
+        console.error(`Configuration is missing the "binary_sensors" entry.`);
+        process.exit(MISSING_BINARY_SENSORS);
+    }
+    else if (!configuration["binary_sensors"])
+    {
+        console.error(`The configuration value for "binary_sensors" is ` +
+                      `required must be an Object.`);
+        process.exit(EMPTY_BINARY_SENSORS);
+    }
+    else if (Object.keys(configuration["binary_sensors"]).length == 0)
+    {
+        console.error(`The configuration value for "binary_sensors" is empty. `+
+                      `It must contain at least one key.`);
+        process.exit(EMPTY_BINARY_SENSORS);
+    }
+
+    let binary_sensor_config_valid = true;
+    for (const name in configuration["binary_sensors"])
+    {
+        const sensor_configuration = configuration["binary_sensors"][name];
+
+        if (sensor_configuration["default_state"] === undefined)
+        {
+            console.error(`Binary sensor configuration for "${name}" is ` +
+                          `missing the "default_state" entry.`);
+            binary_sensor_config_valid = false;
+            continue;
+        }
+
+        const default_state = sensor_configuration["default_state"];
+        if ("on"  != default_state && "off" != default_state)
+        {
+            console.error(`Binary sensor configuration for "${name}" has an ` +
+                          `invalid value for the "default_state" entry. It `  +
+                          `must be one of "on" or "off"`);
+            binary_sensor_config_valid = false;
+            continue;
+        }
+
+        if (sensor_configuration["hosts"] === undefined)
+        {
+            console.error(`Binary sensor configuration for "${name}" is ` +
+                          `missing the "hosts" entry.`);
+            binary_sensor_config_valid = false;
+            continue;
+        }
+        else if (!Array.isArray(sensor_configuration["hosts"]))
+        {
+            console.error(`Binary sensor configuration for "${name}" has an ` +
+                          `invalid value for the "hosts" entry. It must be `  +
+                          `an array containing at least one host string.`);
+            binary_sensor_config_valid = false;
+            continue;
+        }
+        else if (sensor_configuration["hosts"].length == 0)
+        {
+            console.error(`Binary sensor configuration for "${name}" has an `  +
+                          `empty "hosts" entry. It must be an array at least ` +
+                          `one host string.`);
+            binary_sensor_config_valid = false;
+            continue;
+        }
+
+        const hosts = sensor_configuration["hosts"];
+        for (const i in hosts)
+        {
+            if (!hosts[i])
+            {
+                console.error(`Binary sensor configuration for "${name}" ` +
+                              `host entry ${i} is empty. It must be a `    +
+                              `non-empty string.`);
+                binary_sensor_config_valid = false;
+                continue;
+            }
+        }
+    }
+    if (!binary_sensor_config_valid)
+    {
+        process.exit(INVALID_BINARY_SENSORS);
+    }
+
+
+    run(configuration["home_assistant_url"],
+        ha_token,
+        configuration["binary_sensors"],
+        configuration["poll_interval_ms"]);
+}
+
+main(process.argv);
